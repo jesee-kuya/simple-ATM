418bd3d824bb2e4c37fb90263a109870ea7d9974
jkuya <jokumu25@gmail.com>
2025-05-16 13:19:28 +0300

    add deposit and withdraw methods to BankAccount class

--- BankAccount.java
+++ BankAccount.java
@@ -23,7 +23,18 @@ public class BankAccount {
         return balance;
     }
 
- 
- 
+    // Actions
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+        }
+    }
 
+    public boolean withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+            return true;
+        }
+        return false;
+    }
 }
